f1e04406d034c93b4a828d1b446ea3f588a71653
Stanisław Gregor <stanislaw.gregor@idealign.zgora.pl>
2025-08-23 12:55:00 +0200

    content: unify headings

--- src/view-2/view-2.ts
+++ src/view-2/view-2.ts
@@ -4,7 +4,7 @@ angular.module('app.view-2', ['ngRoute']).config([
       template: `
         <div>
           <h1>
-            <code>view-2</code> <small>(this part is being rendered by Angular)</small>
+            View 2 (persistent state)
           </h1>
           <br>
           TODO: Here we will render the Vue app.
